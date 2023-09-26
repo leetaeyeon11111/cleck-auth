309b9424195962a7cca214cf6b4715056813130c
taeyeon <hahaha2662@naver.com>
2023-09-26 12:42:16 +0900

    1st

---
+++ components/Header.jsx
@@ -0,0 +1,77 @@
+import { UserButton, auth } from '@clerk/nextjs'
+import Link from 'next/link'
+
+const Header = () => {
+  const { userId } = auth()
+
+  return (
+    <>
+      <nav className="bg-red-900 py-4 px-8">
+        <div className="flex items-center justify-between container">
+          <div className="flex items-center font-bold">
+            <Link href="/">
+              <div className="text-lg text-white">Clerk3</div>
+            </Link>
+          </div>
+
+          <div className="flex items-center font-bold">
+            {!userId ? (
+              <>
+                <Link
+                  href="/sign-in"
+                  className="text-yellow-300 hover:text-white mr-4"
+                >
+                  Sign In
+                </Link>
+                <Link
+                  href="/courses"
+                  className="text-gray-300 hover:text-white mr-4"
+                >
+                  Courses     
+                </Link>
+
+                <Link
+                  href="/sign-up"
+                  className="text-yellow-300 hover:text-white mr-4"
+                >
+                  Sign Up
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/repos"
+                  className="text-yellow-300 hover:text-white mr-4"
+                >
+                  Repository
+                </Link>
+                <Link
+                  href="/courses"
+                  className="text-yellow-300 hover:text-white mr-4"
+                >
+                  Courses
+                </Link>
+                <Link
+                  href="/dashboard"
+                  className="text-yellow-300 hover:text-white mr-4"
+                >
+                  dashboard
+                </Link>
+                <Link
+                  href="/profile"
+                  className="text-yellow-300 hover:text-white mr-4"
+                >
+                  Profile
+                </Link>
+                <div className="ml-auto">
+                  <UserButton afterSignOutUrl="/" />
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+export default Header
---
+++ components/Repo.jsx
@@ -0,0 +1,45 @@
+import { github_user } from '@/app/const/const'
+import Link from 'next/link'
+import React from 'react'
+import { FaCodeBranch, FaEye, FaStar } from 'react-icons/fa'
+
+const username = github_user
+
+async function fetchRepos(name) {
+  const url = `https://api.github.com/repos/${username}/${name}`
+  const response = await fetch(url)
+  const repo = await response.json()
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  return repo
+}
+
+const Repo = async ({ name }) => {
+  const repo = await fetchRepos(name)
+  const username = 'bradtraversy'
+
+  return (
+    <div>
+      <h3 className="text-xl font-bold">
+        <Link href={`https://api.github.com/repos/${username}/${name}`}>
+          {repo.name}
+        </Link>
+      </h3>
+      <p>{repo.description}</p>   
+      <div className="flex justify-between items-center mb-4">
+             
+        <span className="flex items-center gap-1">
+                    <FaStar /> {repo.stargazers_count}     
+        </span>
+        <span className="flex items-center gap-1">
+                    <FaCodeBranch /> {repo.forks_count}     
+        </span>
+        <span className="flex items-center gap-1">
+                    <FaEye /> {repo.stargazers_count}     
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Repo
